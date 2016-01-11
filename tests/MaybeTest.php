223447478cc5b2a73b532d61a9df989fe4bb920c
Robert Gladson <robert.gladson@martindale.com>
2016-01-11 13:47:29 -0500

    - Implemented a provider for maybe. Resulted in a need to separate the tests between Some and None

--- tests/MaybeTest.php
+++ tests/MaybeTest.php
@@ -13,62 +13,104 @@ use PHPixme as P;
 
 class MaybeTest extends PHPixme_TestCase
 {
-    public function test_Maybe_companion()
+    public function maybeEmptyProvider()
     {
-        $empty = [[], null];
-        foreach ($empty as $value) {
-            $this->assertInstanceOf(
-                P\None
-                , P\Maybe($value)
-                , 'Value ' . json_encode($value, true) . ' should be of type Null'
-            );
-        }
-        $values = [0, '', P\None, false, true, 1, '1'];
-        foreach ($values as $value) {
-            $this->assertInstanceOf(
-                P\Some
-                , P\Maybe($value)
-                , 'Value ' . json_encode($value, true) . ' should be of type Some'
-            );
-        }
+        return [
+            [[]]
+            , [null]
+        ];
+    }
+
+    public function maybeSomethingProvider()
+    {
+        return [
+            [0]
+            , ['']
+            , [P\None()]
+            , [false]
+            , [true]
+            , [1]
+            , [1.1]
+            , ['1']
+            , [[1]]
+            , [new \stdClass()]
+            , [P\Some('')]
+        ];
+    }
+
+
+    /**
+     * @dataProvider maybeEmptyProvider
+     */
+    public function test_Maybe_companion_none_result($value)
+    {
+        $this->assertInstanceOf(
+            P\None
+            , P\Maybe($value)
+            , 'Value ' . json_encode($value, true) . ' should be of type None'
+        );
+    }
+
+    /**
+     * @dataProvider maybeSomethingProvider
+     */
+    public function test_Maybe_companion_some_result($value)
+    {
+        $this->assertInstanceOf(
+            P\Some
+            , P\Maybe($value)
+            , 'Value ' . json_encode($value, true) . ' should be of type Some'
+        );
+    }
+
+    /**
+     * @dataProvider maybeEmptyProvider
+     */
+    public function test_static_of_nothing($value)
+    {
+        $this->assertInstanceOf(
+            P\None
+            , P\Maybe::of($value)
+            , 'Value ' . json_encode($value, true) . ' should be of type Null'
+        );
+    }
+
+    /**
+     * @dataProvider maybeSomethingProvider
+     */
+    public function test_static_of_something($value)
+    {
+
+        $this->assertInstanceOf(
+            P\Some
+            , P\Maybe::of($value)
+            , 'Value ' . json_encode($value, true) . ' should be of type Some'
+        );
+    }
+
+    /**
+     * @dataProvider maybeEmptyProvider
+     */
+    public function test_static_from_nothing($value)
+    {
+
+        $this->assertInstanceOf(
+            P\None
+            , P\Maybe::from($value)
+            , 'Value ' . json_encode($value, true) . ' should be of type Null'
+        );
 
     }
 
-    public function test_static_of() {
-        $empty = [[], null];
-        foreach ($empty as $value) {
-            $this->assertInstanceOf(
-                P\None
-                , P\Maybe::of($value)
-                , 'Value ' . json_encode($value, true) . ' should be of type Null'
-            );
-        }
-        $values = [0, '', P\None, false, true, 1, '1'];
-        foreach ($values as $value) {
-            $this->assertInstanceOf(
-                P\Some
-                , P\Maybe::of($value)
-                , 'Value ' . json_encode($value, true) . ' should be of type Some'
-            );
-        }
-    }
-
-    public function test_static_from() {
-        $empty = [[], null];
-        foreach ($empty as $value) {
-            $this->assertInstanceOf(
-                P\None
-                , P\Maybe::from($value)
-                , 'Value ' . json_encode($value, true) . ' should be of type Null'
-            );
-        }
-        $values = [0, '', P\None, false, true, 1, '1'];
-        foreach ($values as $value) {
-            $this->assertInstanceOf(
-                P\Some
-                , P\Maybe::from($value)
-                , 'Value ' . json_encode($value, true) . ' should be of type Some'
-            );
-        }
+    /**
+     * @dataProvider maybeSomethingProvider
+     */
+    public function test_static_from_something($value)
+    {
+        $this->assertInstanceOf(
+            P\Some
+            , P\Maybe::from($value)
+            , 'Value ' . json_encode($value, true) . ' should be of type Some'
+        );
     }
 }
