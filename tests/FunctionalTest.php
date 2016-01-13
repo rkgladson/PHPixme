61643dd99f774693418e893552ae49e3df7eb0c1
Robert Gladson <robert.gladson@martindale.com>
2016-01-13 15:18:07 -0500

    - Unit testing Starling

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -300,4 +300,35 @@ class FunctionalTest extends PHPixme_TestCase
             , 'The notoriously simple idiot bird proves useful in unusual places'
         );
     }
+
+    public function test_S_tarling($value = true) {
+        $this->assertStringEndsWith(
+            '\S'
+            , P\S
+            , 'Ensure the constant is assigned to its function name'
+        );
+        $this->assertInstanceOf(
+            Closure
+            , P\S(P\I)
+            , 'Starling should be able to be partially applied'
+        );
+        $this->assertEquals(
+            $value
+            , P\S(P\K)->__invoke(P\K($value))->__invoke($value)
+            , 'S(K, K($value))($value) === $value is one of the more basic proofs to Starling'
+        );
+
+    }
+
+    public function test_S_tarling_scenario_tupleMaker ($array = [1,2,3,4]) {
+        // Test to see if we can fix array_map through starling to get the key with the value
+        $kvTupple = function ($v, $k) {
+            return [$k, $v];
+        };
+        $kvMap = P\ternary('array_map')->__invoke($kvTupple);
+        $this->assertEquals(
+            array_map($kvTupple, $array, array_keys($array))
+            ,P\S($kvMap, 'array_keys')->__invoke($array)
+        );
+    }
 }
