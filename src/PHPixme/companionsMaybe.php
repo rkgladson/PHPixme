f5dfb88d9e4fa5c14016ddcd253df669b94ce30b
Robert Gladson <robert.gladson@martindale.com>
2016-04-08 14:10:05 -0400

    - Formatting and annotation for Maybe and Seq companion functions.

--- src/PHPixme/companionsMaybe.php
+++ src/PHPixme/companionsMaybe.php
@@ -1,6 +1,6 @@
 <?php
 namespace PHPixme;
-const Maybe = __NAMESPACE__.'\Maybe';
+const Maybe = __NAMESPACE__ . '\Maybe';
 /**
  * Takes a value and wraps it in a Maybe family object
  * @param $x - the maybe existing value
@@ -8,28 +8,29 @@ const Maybe = __NAMESPACE__.'\Maybe';
  */
 function Maybe($x = null)
 {
-    return (
-        !isset($x) || is_null($x) ||
-        (is_array($x) && count($x) === 0)
-    ) ?
-        None()
-        : Some($x);
+  return (
+    !isset($x) || is_null($x) ||
+    (is_array($x) && count($x) === 0)
+  ) ? None()
+    : Some($x);
 }
 
 
-
-const None = __NAMESPACE__.'\None';
+const None = __NAMESPACE__ . '\None';
+/**
+ * @return \PHPixme\None
+ */
 function None()
 {
-    return None::getInstance();
+  return None::getInstance();
 }
 
-const Some = __NAMESPACE__.'\Some';
+const Some = __NAMESPACE__ . '\Some';
 /**
  * @param $x - a non- null value
  * @return \PHPixme\Some
  */
 function Some($x)
 {
-    return new Some($x);
+  return new Some($x);
 }
--- src/PHPixme/companionsSeq.php
+++ src/PHPixme/companionsSeq.php
@@ -1,9 +1,12 @@
 <?php
 namespace PHPixme;
 
-
-const Seq = __NAMESPACE__.'\Seq';
+const Seq = __NAMESPACE__ . '\Seq';
+/**
+ * @param $arrayLike
+ * @return \PHPixme\Seq
+ */
 function Seq($arrayLike)
 {
-    return new Seq($arrayLike);
+  return new Seq($arrayLike);
 }
