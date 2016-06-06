c6c3f88b0495f5f3511276d1cb50aac9cabc3607
Robert Gladson <robert.gladson@martindale.com>
2016-06-06 11:08:47 -0400

    ClosedTrait unit test Coverage

--- tests/ClosedTraitTest.php
+++ tests/ClosedTraitTest.php
@@ -12,22 +12,37 @@ namespace tests\PHPixme;
 use PHPixme\ClosedTrait;
 use PHPixme\exception\MutationException;
 
+/**
+ * Class ClosedTraitTest
+ * @package tests\PHPixme
+ * @coversDefaultClass PHPixme\ClosedTrait
+ */
 class ClosedTraitTest extends \PHPUnit_Framework_TestCase
 {
+  /**
+   * @coversNothing
+   */
+  function test_get_set_final() {
+    $reflection = new \ReflectionClass(ClosedTrait::class);
+    $this->assertTrue($reflection->getMethod('__get')->isFinal());
+    $this->assertTrue($reflection->getMethod('__set')->isFinal());
+  }
+
+  /**
+   * @covers ::__get
+   */
   function test_exception_on_get_dynamic() {
     $this->expectException(MutationException::class);
     $obj = new CloseTypeStub();
     $obj->exceptionFoo;
   }
 
+  /**
+   * @covers ::__set
+   */
   function test_exception_on_set_dynamic() {
     $this->expectException(MutationException::class);
     $obj = new CloseTypeStub();
     $obj->exceptionFoo = 'bar';
-  }
-  function test_get_set_final() {
-    $reflection = new \ReflectionClass(ClosedTrait::class);
-    $this->assertTrue($reflection->getMethod('__get')->isFinal());
-    $this->assertTrue($reflection->getMethod('__set')->isFinal());
   }
 }
